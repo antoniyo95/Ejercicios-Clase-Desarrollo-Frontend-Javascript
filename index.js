8a02f59bcf5593d520a928992fb1dcacd4610351
Antonio Luis Martinez <antonillo_1995@hotmail.com>
2023-03-07 19:19:06 +0100

    Subo al repo lo cursado en los dias 1,2,3 y 4

---
+++ index.js
@@ -0,0 +1,13 @@
+import { notificationController } from "./notifications/notificationController.js"
+import { tweetListController } from "./tweet-list/tweetListController.js"
+
+const tweetListElement = document.querySelector('.tweet-list')
+const notificationsElement = document.querySelector('.notifications')
+
+const showMessage = notificationController(notificationsElement)
+tweetListController(tweetListElement)
+
+tweetListElement.addEventListener('newNotification', (event) => {
+  console.log('He recibido el evento!!!! ', event.detail.message);
+  showMessage(event.detail.message)
+})
---
+++ signup/signupController.js
@@ -0,0 +1,46 @@
+import { createUser } from "./signup.js";
+
+export function signupController(signupElement) {
+
+  signupElement.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const emailElement = signupElement.querySelector('#username');
+    const passwordElement = signupElement.querySelector('#password');
+    const passwordConfirmElement = signupElement.querySelector('#passwordConfirm');
+
+    if (isEmailValid(emailElement.value) &&
+        isPasswordValid(passwordElement.value, passwordConfirmElement.value)) {
+          try {
+            await createUser(emailElement.value, passwordElement.value)
+            signupElement.reset();
+            alert('Usuario creado correctamente')
+            window.location = '/'
+          } catch (error) {
+            alert(error.message)
+          }
+    }
+  })
+
+  function isEmailValid(email) {
+    const mailRegExp = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+
+    if (!mailRegExp.test(email)) {
+      alert('El email no está bien escrito');
+      return false
+    }
+
+    return true
+  }
+
+  function isPasswordValid(password, passwordConfirmation) {
+    if (password !== passwordConfirmation) {
+      alert('Las contraseñas no son iguales');
+      return false
+    }
+
+    return true
+  }
+}
+
+
